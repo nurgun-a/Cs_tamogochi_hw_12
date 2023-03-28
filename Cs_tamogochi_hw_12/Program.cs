a1a024dcae449e54da0e60b446ceb8c54d0d7ca4
nurgun_a <nurgun.alekseev@bk.ru>
2023-03-28 03:00:24 +0300

    Добавьте файлы проекта.

---
+++ Cs_tamogochi_hw_12/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Threading;
+
+namespace Cs_tamogochi_hw_12
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int count1 = 1;
+            User u1 = new User();
+            Tamagochi t1 = new Tamagochi();
+            Write($"Назовите вашего питомца: ");
+            string str = ReadLine();
+            t1.Name = str;
+            WriteLine();
+            while (true)
+            {
+                Clear();
+                WriteLine($"{t1}");
+                Thread.Sleep(1000);
+                Game(ref t1, ref count1);
+                count1++;
+            }
+           
+        }
+        public static void Game(ref Tamagochi tam, ref int n)
+        {
+            Random random = new Random();
+            User u1 = new User();
+            u1.FeedEvent += tam.Feed;
+            u1.PlayEvent += tam.Play;
+            u1.Take_outEvent += tam.Take_out;
+            u1.DeadEvent += tam.Dead;
+            u1.HealEvent += tam.Heal;
+            if (tam.Count_ > 2)
+            {
+                u1.Heal();
+            }
+            else if (tam.Health == 0)
+            {
+                u1.Dead();
+            }
+            else
+            {
+                switch (n)
+                {
+                    case 1:
+                        u1.Feed();
+                        break;
+                    case 2:
+                        u1.Take_out();
+                        break;
+                    case 3:
+                        u1.Play();
+                        break;
+                }
+                if (n == 3) n = 0;
+            }
+
+        }
+    }
+    
+}
---
+++ Cs_tamogochi_hw_12/Tamagochi.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Windows.Forms;
+
+namespace Cs_tamogochi_hw_12
+{
+    public delegate void FeedDelegate();
+    public delegate void Take_outDelegate();
+    public delegate void DeadDelegate();
+    public delegate void HealDelegate();
+    public delegate void PlayDelegate();
+    class Tamagochi
+    {
+        public int Health { get; set; } = 100;
+        public string Name { get; set; }
+        public int Count_ { get; set; } = 0;
+        public void Feed()
+        {
+            if (MessageBox.Show($"Накормить {Name}", $"Ваш питомец хочет кушать",
+               MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                if (Health < 100) Health += 20;               
+                
+            }
+            else
+            {
+                if (Health > 0) Health -= 20;
+                Count_++;
+            }
+           
+        }
+        public void Take_out()
+        {
+            if (MessageBox.Show($"Погулять с {Name}", $"Ваш питомец хочет гулять ",
+                MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                if (Health < 100) Health += 20;
+            }
+            else
+            {
+                if (Health > 0) Health -= 20;
+                Count_++;
+            }
+        }
+        public void Play()
+        {
+            if (MessageBox.Show($"Поиграть с {Name}", $"Ваш питомец хочет поиграть",
+               MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                if (Health < 100) Health += 20;
+
+            }
+            else
+            {
+                if (Health > 0) Health -= 20;
+                Count_++;
+            }
+
+        }
+        public void Heal()
+        {
+            if (MessageBox.Show($"Вылечить {Name}", $"Ваш питомец умирает!!!",
+                MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                if (Health < 100) Health =100;
+                Count_ = 0;
+            }
+            else
+            {                
+                Dead();
+            }
+        }
+        public void Dead()
+        {
+            Clear();
+            Health = 0;
+            WriteLine($"{Name} УМИР!!!");
+            WriteLine($"{ToString()}");
+            Environment.Exit(0);
+        }
+        public override string ToString()
+        {
+            if (Health==0)
+            {
+            return $"Health: {Health}\nName: {Name}" + @"
+                 _
+              (\   /)
+              (=x.x=)
+              () _ ()
+               \_|_/";
+            }
+            else return $"Health: {Health}\nName: {Name}"+@"
+                 _
+              (\   /)
+              (='.'=)
+              () _ ()
+               \_|_/"; 
+        }
+    }
+
+    class User
+    {  
+        public event FeedDelegate FeedEvent;
+        public event Take_outDelegate Take_outEvent;
+        public event HealDelegate HealEvent;
+        public event DeadDelegate DeadEvent;
+        public event DeadDelegate PlayEvent;
+        public void Feed()
+        {
+            if (FeedEvent != null)
+            {
+                FeedEvent();
+            }
+        }
+        public void Take_out()
+        {
+            if (Take_outEvent != null)
+            {
+                Take_outEvent();
+            }
+        }
+        public void Play()
+        {
+            if (PlayEvent != null)
+            {
+                PlayEvent();
+            }
+        }
+        public void Heal()
+        {
+            if (HealEvent != null)
+            {
+                HealEvent();
+            }
+        }
+        public void Dead()
+        {
+            if (DeadEvent != null)
+            {
+                DeadEvent();
+            }
+        }
+    }
+}
